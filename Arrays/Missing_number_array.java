9019c2f7ed8d0818e0afc3f02796e85959cf611d
Patel Jay <pateljay51003@gmail.com>
2025-01-24 17:21:07 +0530

    Missing_Number_array

---
+++ Arrays/Missing_number_array.java
@@ -0,0 +1,33 @@
+package Arrays;
+
+// Given an array of size N-1 such that it only contains distinct integers in the range of 1 to N. Find the missing element.
+public class Missing_number_array {
+    public static int missingNumber(int[] a, int N) {
+
+        int hash[] = new int[N + 1]; // hash array
+
+        // storing the frequencies:
+        for (int i = 0; i < N - 1; i++)
+            hash[a[i]]++;
+
+        // checking the freqencies for numbers 1 to N:
+        for (int i = 1; i <= N; i++) {
+            if (hash[i] == 0) {
+                return i;
+            }
+        }
+
+        // The following line will never execute.
+        // It is just to avoid warnings.
+        return -1;
+    }
+
+    public static void main(String args[]) {
+        int N = 5;
+        int a[] = { 1, 2, 4, 5 };
+
+        int ans = missingNumber(a, N);
+        System.out.println("The missing number is: " + ans);
+    }
+
+}
